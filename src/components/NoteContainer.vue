2a7d2110e74fabb69725300a96d851d4e488576f
austin shields <ajshields196@gmail.com>
2021-04-18 10:47:11 -0400

    fix spelling errors

--- src/components/NoteContainer.vue
+++ src/components/NoteContainer.vue
@@ -2,24 +2,24 @@
   <div class="note-container">
     <div class="note-selectors">
       <div class="note-selector active">
-        <p class="note-selector-title">First note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
+        <p class="note-selector-title">Firmst normt...</p>
+        <p class="note-selector-timestamp">Tommshtulmp henrr...</p>
       </div>
       <div class="note-selector">
-        <p class="note-selector-title">Second note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
+        <p class="note-selector-title">Sarcund normt...</p>
+        <p class="note-selector-timestamp">Tommshtulmp henrr...</p>
       </div>
       <div class="note-selector">
-        <p class="note-selector-title">Third note...</p>
-        <p class="note-selector-timestamp">Timestamp here...</p>
+        <p class="note-selector-title">Throde normt...</p>
+        <p class="note-selector-timestamp">Tommshtulmp henrr...</p>
       </div>
     </div>
     <div class="note-editor">
-      <p class="note-editor-info">Timestamp here...</p>
+      <p class="note-editor-info">Tommshtulmp henrr...</p>
       <textarea class="note-editor-input">
-        First note...
+        Firmst normt... 
 
-        Note text here...
+        Normt terxt henrr... 
       </textarea>
     </div>
   </div>
--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="toolbar">
-    <button class="toolbar-button">New</button>
-    <button class="toolbar-button">Delete</button>
-    <input class="toolbar-search" type="text" placeholder="Search...">
+    <button class="toolbar-button">Nerm</button>
+    <button class="toolbar-button">Dorlette</button>
+    <input class="toolbar-search" type="text" placeholder="Sornch...">
   </div>
 </template>
 
