991d43846dce78986935a82144583e08083d29d4
austin shields <ajshields196@gmail.com>
2021-04-18 10:40:34 -0400

    move relevant CSS into components

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -5,3 +5,36 @@
     <input class="toolbar-search" type="text" placeholder="Search...">
   </div>
 </template>
+
+<style scoped>
+
+/* LAYOUT */
+
+.toolbar {
+  padding: 0.5em;
+}
+
+.toolbar-button, .toolbar-search {
+  padding: inherit;
+  border-radius: 0.3em;
+}
+
+.toolbar-search {
+  float: right;
+}
+
+/* COLORS */
+
+.toolbar {
+  background-color: #DCDADC;
+}
+
+.toolbar-button {
+  background-color: #FFFFFF;
+}
+
+.toolbar-button:active {
+  background-color: #AAAAAA;
+}
+
+</style>
